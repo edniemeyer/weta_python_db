a8ecb2e52dadc976579e7b134cbf5e7339a96f71
eduardo <edniemeyer@gmail.com>
2019-02-12 19:53:46 +1300

    Saving unique rows (PROJECT,SHOT,VERSION) to datastore as "id"

--- importer.py
+++ importer.py
@@ -1,8 +1,20 @@
 import csv
 
 # opening data file and putting it into a Dict
-with open("data.txt","r") as data:
-	reader = csv.DictReader(data, delimiter="|")
-	for row in reader:
-		print row
+with open("data.txt","r") as file:
+	data = csv.DictReader(file, delimiter="|")
+	new_data=[]
+	for row in data:
+		row.update({"id":row['PROJECT']+row['SHOT']+row['VERSION']})
+		new_data.append(row)
+	#map(lambda d: d['PROJECT']+d['SHOT']+d['VERSION'], data)
+	new_data_unique = {v["id"]:v for v in new_data}.values()
 
+
+
+with open("db.csv","w",) as output:
+	fieldnames = ['id','PROJECT','SHOT','VERSION','STATUS','FINISH_DATE','INTERNAL_BID','CREATED_DATE']
+	writer = csv.DictWriter(output, fieldnames=fieldnames)
+	writer.writeheader()
+	for row in new_data_unique:
+		writer.writerow(row)
